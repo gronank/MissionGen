3874cc25670443bbfff1803367101b0f87e4f05c
anders ryndel <anders_ryndel@hotmail.com>
2021-05-07 00:30:59 +0200

    update requirements

--- MissionGen/MissionGen.py
+++ MissionGen/MissionGen.py
@@ -1,5 +1,6 @@
 import pandas as pa
 import numpy as np
+import dcs
 from FlightData import loadFlightData
 filename="../TestData/AirGoons DCS Weekly Package.ods"
 signup="Sign-Ups"
@@ -7,4 +8,6 @@ sheet=pa.read_excel(filename,sheet_name=signup,engine="odf").to_numpy()
 
 flightData = loadFlightData(sheet)
 
+m=dcs.Mission()
+m.remove_plane_group
 a=8
