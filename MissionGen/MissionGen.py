0d658160eb524e2d143dfd8c5aee94e5ac726209
anders ryndel <anders_ryndel@hotmail.com>
2021-05-06 22:19:08 +0200

    sheet reading

--- MissionGen/MissionGen.py
+++ MissionGen/MissionGen.py
@@ -1,1 +1,8 @@
-
+import pandas as pa
+import numpy as np
+filename="../TestData/AirGoons DCS Weekly Package.ods"
+signup="Sign-Ups"
+sheet=pa.read_excel(filename,sheet_name=signup,engine="odf").to_numpy()
+flightIndices=list(zip(*np.where(sheet=="Tasking")))
+flightData = [readFlightData(sheet,index) for index in flightIndices]
+a=8
