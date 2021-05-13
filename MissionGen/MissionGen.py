eda2cc9c9051d120d3a27a96136a0754fe34c593
anders ryndel <anders_ryndel@hotmail.com>
2021-05-13 16:51:32 +0200

    mission gen

--- MissionGen/MissionGen.py
+++ MissionGen/MissionGen.py
@@ -6,16 +6,21 @@ from dcs import Mission
 from PackageHandler import PackageHandler
 filename="../TestData/AirGoons DCS Weekly Package.ods"
 templateMission="../TestData/GoonTest1.miz"
+testOut="../TestData"
 signup="Sign-Ups"
-sheet=pa.read_excel(filename,sheet_name=signup,engine="odf").to_numpy()
 
-flightData = loadFlightData(sheet)
+def generateMission(sheetPath:str,templateMissionPath:str,outDir:str):
+    sheet=pa.read_excel(sheetPath,sheet_name=signup,engine="odf").to_numpy()
+    flightData = loadFlightData(sheet)
 
 
-mission=Mission()
-mission.load_file(templateMission)
-package=PackageHandler(sheet, mission)
-for flight in flightData:
-    generateGroup(mission,flight, package)
-mission.save("../TestData/Test.miz")
+    mission=Mission()
+    mission.load_file(templateMissionPath)
+    package=PackageHandler(sheet, mission)
+    for flight in flightData:
+        generateGroup(mission,flight, package)
+    mission.save(f"{outDir}/{package.package_name}.miz")
+if __name__=="__main__":
+    generateMission(filename, templateMission, testOut)
+
 
---
+++ readme.md
@@ -0,0 +1,7 @@
+## AirGoon Mission generation
+
+### Description
+
+This is a script that generates goon missions based on a mission template combined with the sign-up sheet. 
+
+The idea is to use the mission editor to create the mission template with all the enemies, vehicles, refuelers and what-have-you. But this is not a good way of handling the less static and more fickle aspects of the mission. Of course, the most fickle aspect is the goons themselves, occasionally distracted by something they call 'life'. To make organizing a mission not a soul crushing experience, all the things that might get adjusted along the way such as participants and radio frequencies are added on top of the template to create the finished mission. 
